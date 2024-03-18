a35007965018b93363abacdd31158f8d5bea438f
Arash Sadeghi Amjadi <arashsadeghiamjadi@smus-m605.wds.mun.ca>
2024-03-17 23:58:31 -0230

    Updates

---
+++ precache-manifest.5ea6ef40c9a6e34b59f32667060a9ff4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9682ae17984c64276ac37c8a7e849f",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "50bd5fdb752a319aec74",
+    "url": "/portfolio-website/static/css/main.239fc7fd.chunk.css"
+  },
+  {
+    "revision": "a2e6f317feea31adfe90",
+    "url": "/portfolio-website/static/js/2.c7e1e596.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/portfolio-website/static/js/2.c7e1e596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bd5fdb752a319aec74",
+    "url": "/portfolio-website/static/js/main.6c6141e1.chunk.js"
+  },
+  {
+    "revision": "9acb84acd13b69e2bf79",
+    "url": "/portfolio-website/static/js/runtime-main.bc6b0371.js"
+  },
+  {
+    "revision": "1d9a9a61b165371819fdb401e46dd6e6",
+    "url": "/portfolio-website/static/media/BabyLizardsScreenshot.1d9a9a61.png"
+  },
+  {
+    "revision": "ba90139b10ec72d9ebd0cecac4ea16cc",
+    "url": "/portfolio-website/static/media/BriannaMcDonaldDissertation.ba90139b.pdf"
+  },
+  {
+    "revision": "17d374fad0546122373fa62208a316e3",
+    "url": "/portfolio-website/static/media/Resume.17d374fa.pdf"
+  },
+  {
+    "revision": "79501f3f4e1acca65988e630ed693196",
+    "url": "/portfolio-website/static/media/SobelScreenshot.79501f3f.png"
+  },
+  {
+    "revision": "a4ec4d6f0a6b25b1092cea35fd4718bc",
+    "url": "/portfolio-website/static/media/ToDoListGif.a4ec4d6f.gif"
+  },
+  {
+    "revision": "2e6e4f69808a98b7aecbb874ff21d526",
+    "url": "/portfolio-website/static/media/background.2e6e4f69.png"
+  },
+  {
+    "revision": "75a53ff2e6c25fb7bc3a9cf0f691fcf9",
+    "url": "/portfolio-website/static/media/calculatorGif.75a53ff2.gif"
+  },
+  {
+    "revision": "9ec3576b86e1b463b6d853084558e3f0",
+    "url": "/portfolio-website/static/media/covidGif.9ec3576b.gif"
+  },
+  {
+    "revision": "949a49b48e58e70119574e41de7b1ff4",
+    "url": "/portfolio-website/static/media/cssart.949a49b4.jpg"
+  },
+  {
+    "revision": "0887e2e7e41a182f39f6dd4bda1dda80",
+    "url": "/portfolio-website/static/media/gdmcGif.0887e2e7.gif"
+  },
+  {
+    "revision": "e2cf43dd1241d30343704ea24be09fdd",
+    "url": "/portfolio-website/static/media/hackFrostGif.e2cf43dd.gif"
+  },
+  {
+    "revision": "ea7076dfdd6ae3a8c9867a127f7720da",
+    "url": "/portfolio-website/static/media/honours.ea7076df.gif"
+  },
+  {
+    "revision": "85a57f9184a6919783f2726999e5c165",
+    "url": "/portfolio-website/static/media/me.85a57f91.png"
+  },
+  {
+    "revision": "ae2e9e737ad76275de0f1e7b74ea626a",
+    "url": "/portfolio-website/static/media/megaMarioScreenshot.ae2e9e73.png"
+  },
+  {
+    "revision": "3f2dee47766e427f2ab91c6b3319a692",
+    "url": "/portfolio-website/static/media/prims.3f2dee47.gif"
+  },
+  {
+    "revision": "7600ca438cdc3a6b685ab91089f28ff2",
+    "url": "/portfolio-website/static/media/sqliteLogo.7600ca43.png"
+  },
+  {
+    "revision": "52b67810dbd8be3de77988b123aa29b9",
+    "url": "/portfolio-website/static/media/thresholdingScreenshot.52b67810.png"
+  }
+]);
